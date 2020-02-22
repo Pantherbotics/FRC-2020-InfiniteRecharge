f01bb5c7e46960fee76b5e3a296189768314d939
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2020-02-21 19:00:29 -0800

    fix

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -9,48 +9,46 @@ import edu.wpi.first.wpilibj.I2C;
 
 // Other imports
 import edu.wpi.first.wpilibj.Notifier;
-import frc.robot.Target;
 
 /*
     Interface with raspberry pi over i2c connection
 */
-public class Vision extends Subsystem
+public class Vision
 {
     /*
         arbitrary i2c constants
     */
     
     // the address of raspberry pi on i2c port
-    private static final int kAddress = 0x69;
-    
+    private final int kAddress = 0x69;
+
     // command registers to let PI know what we're tryna do
-    private static final byte kRegisterBegin        = 0xa0;
-    private static final byte kRegisterGetData      = 0xa1;
-    private static final byte kRegisterExposureTime = 0xa2;
-    
+    private final byte kRegisterBegin = (byte) 0xa0;
+    private final byte kRegisterGetData = (byte) 0xa1;
+    private final byte kRegisterExposureTime = (byte) 0xa2;
+
     // seconds of exposure = kExposureTime / 10,000
     public byte kExposureTime = 10;
-    
+
     /*
-        Helpers
-    */
+     * Helpers
+     */
     private I2C mI2C;
     private Target mTarget;
     private Notifier updateLoop;
-    
+
     /*
-        Variables
-    */
-    private void updateFailCount = 0;
-    
+     * Variables
+     */
+    private int updateFailCount = 0;
+
     /*
-        Constructor
-    */
-    public Vision(I2C.Port physicalPort)
-    {
+     * Constructor
+     */
+    public Vision(I2C.Port physicalPort) {
         // initiate target
         this.mTarget = new Target();
-        
+
         // open i2c port
         this.mI2C = new I2C(physicalPort, this.kAddress);
         
@@ -58,8 +56,13 @@ public class Vision extends Subsystem
         this.mI2C.write(kRegisterBegin, (byte) 0xff);
         
         // create update loop
-        this.updateLoop = new Notifier(this.update);
+        this.updateLoop = new Notifier(() -> {
+            this.update();
+        });
+        
+        System.out.println("begin loop");
         this.updateLoop.startPeriodic(20f / 1000f);
+        
     }
         
     /*
@@ -69,27 +72,51 @@ public class Vision extends Subsystem
     private boolean updateTarget()
     {
         // create buffer
-        ByteBuffer rawData = ByteBuffer.allocate(4);
-        
+        System.out.println("loop");
+        byte[] rawData = new byte[10];
+        boolean aborted = this.mI2C.read(0, 10, rawData);
+
         // if the read is aborted
-        if (this.mI2C.read(this.kRegisterGetData, 4, rawData))
-        {
+        if (aborted)
             return false;
+
+        boolean found = false;
+        int dataStart;
+        for (dataStart = 0; dataStart < 6; dataStart++)
+        {
+            if (rawData[dataStart] == (byte)0x69)
+            {
+                found = true;
+                break;
+            }
         }
+
+        if (!found)
+            return false;
         
-        // data is returned in little endian
-        rawData.order(ByteOrder.LITTLE_ENDIAN);
+        byte[] byteData = {rawData[dataStart + 1], rawData[dataStart + 2], rawData[dataStart + 3], rawData[dataStart + 4]};
+        
+        // data is returned in little endian, convert to big (so my small brain can understand)
+        int[] distance_raw = {byteData[1] & 0xff, byteData[0] & 0xff};
+        int[] horizontal_raw = {byteData[3] & 0xff, byteData[2] & 0xff};
+        int distance = (distance_raw[0] << 8) | distance_raw[1];
+        int horizontal = (horizontal_raw[0] << 8) | horizontal_raw[1];
+        // there is probably a better way of reading little endian unsigned shorts, but idc
+        System.out.println("Sent: " + String.format("0x%02X ", byteData[0]) + ", "
+                                    + String.format("0x%02X ", byteData[1]) + ", "
+                                    + String.format("0x%02X ", byteData[2]) + ", "
+                                    + String.format("0x%02X ", byteData[3]));
         
         // struct Target {
         //     unsigned short distance;
         //     unsigned short horizontal;
         // }
-        this.mTarget.distance   = (int) (rawData.getShort(0) & 0xffff);
-        this.mTarget.horizontal = (int) (rawData.getShort(2) & 0xffff);
+        this.mTarget.distance   = distance;
+        this.mTarget.horizontal = horizontal;
         
         // target.exists = either is nonzero
-        this.mTarget.exists = (this.mTarget.distance != 0) || (this.mTarget.horizontal != 0)
-        
+        this.mTarget.exists = (this.mTarget.distance != 0) || (this.mTarget.horizontal != 0);
+        System.out.println("Distance: " + this.mTarget.distance + ", Angle: " + this.mTarget.horizontal);
         return true;
     }
     
@@ -98,12 +125,12 @@ public class Vision extends Subsystem
     */
     public void update()
     {
-        boolean success = true;
         try
         { // exit if update fails for any reason
-            assert this.updateTarget();
+            boolean success = this.updateTarget();
+            assert success : "Failed to update target, but this err will be caught so who cares";
         }
-        catch Exception e
+        catch(Exception e)
         { // update has failed
             this.updateFailCount++;
 
@@ -130,7 +157,3 @@ public class Vision extends Subsystem
         return this.mTarget;
     }
 }
-
-
-
-
